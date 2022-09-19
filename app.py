da51d6726689b970cad9e3605a72e8ebe751fcb4
ubs <ubs4939@naver.com>
2022-09-19 21:58:39 +0900

    init

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template, jsonify, request
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def parts():
+    return render_template('index.html',
+    title = '세탁자리구함')
+
+
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000, debug=True)
