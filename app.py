ca3650879dc970427e6e999b4a99e11e8b61eb4c
ubs <ubs4939@naver.com>
2022-09-20 16:02:56 +0900

    app joinMember 연결

--- app.py
+++ app.py
@@ -4,11 +4,15 @@ from flask import Flask, render_template, jsonify, request
 app = Flask(__name__)
 
 @app.route('/')
-def parts():
+def main():
     return render_template('index.html',
     title = '세탁자리구함')
 
 
+@app.route('/joinMember', methods=['GET'])
+def join():
+    return render_template('join.html',
+    title = '회원가입')
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
